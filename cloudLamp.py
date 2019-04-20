c8a498a75f5bfb44f09037f4749f9d4e6f5aa223
syeomans <samuelyeomans@gmail.com>
2019-04-20 14:57:32 -0400

    Created getWeather function

--- cloudLamp.py
+++ cloudLamp.py
@@ -1,40 +1,26 @@
-# This script controlls the relay attached to my alarm clock lamp.
-
 # Import modules
-#import time
-import RPi.GPIO as GPIO
-#from datetime import datetime
-from relay import Relay
-import fns
-import states
-from globalvars import *
+# import time
+# import RPi.GPIO as GPIO
+import urllib2
+import json
 
-# Pin setup
-GPIO.setup(PB1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(PB2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(PB3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+def getWeather():
+	"""
+	Make an api call to openweathermap.org
 
-# Create lamp, an object of Relay class, from pin 11 (RELAY)
-lamp = Relay(RELAY)
+	Returns one of:
+	['Clear', 'Drizzle', 'Clouds', 'Thunderstorm', 'Rain', 'Snow', 'Fog', 'Mist', 'Smoke', 'Haze', 'Ash']
+		(Technically it can return more, but these are the only ones we care about. 
+		Link to see the rest: https://openweathermap.org/weather-conditions )
+	"""
+	apiKey = "1be18276dbf6dd7f92eb0d144ef88985"
+	url = "https://api.openweathermap.org/data/2.5/weather?id=4671576&APPID=" + apiKey
 
-# Blink once at the beginning to show the script is working 
-fns.blink(lamp, 1)
+	request = urllib2.Request(url)
+	contents = json.load(urllib2.urlopen(request))
+	return(contents['weather'][0]['main'])
 
-try:
-	# Main loop
-	running = True
-	thisState = "OnOff"
-	while running: 
-		thisState = states.getNextState(thisState, lamp)
+# Main loop
+currentWeather = getWeather()
+print(currentWeather)
 
-# If keyboard interrupt
-except KeyboardInterrupt:
-	GPIO.output(RELAY, GPIO.LOW)
-	
-# Exit cleanly
-finally:
-
-	# Blink quickly twice, then exit cleanly
-	fns.blink(lamp, 2, 0.5)
-	
-	GPIO.cleanup()
