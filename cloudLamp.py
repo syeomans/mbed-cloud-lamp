1ed819a02626f2501378673521a7e31025ccd4c9
syeomans <samuelyeomans@gmail.com>
2019-04-23 13:07:41 -0400

    Serial test 2

--- cloudLamp.py
+++ cloudLamp.py
@@ -27,6 +27,6 @@ def getWeather():
 
 ser = serial.Serial('/dev/ttyACM0')  # open serial port
 print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
+ser.write('1')     # write a string
 ser.close()             # close port
 
