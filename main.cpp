541d930bf517555fb9a00482b0e4e30c0547cf5e
seleswarapu3 <43051654+seleswarapu3@users.noreply.github.com>
2019-04-23 16:03:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 PwmOut red(p23);
 PwmOut green(p21);
 PwmOut blue(p22);
-
+RawSerial  pi(USBTX, USBRX);
+char temp;
 
 void fade(float r, float g, float b, float stepSize, float delay){
     int rDir;
@@ -50,15 +51,15 @@ void rain(float r, float g, float b, float stepSize, float duration) {
     float step = 0.0;
     while (step < duration) {
         if (red == r && green == g && blue == b) {
-            red = 0.0;
-            green = 0.0;
-            blue = 0.0;
-            wait(0.25);
+            red = r;
+            green = g;
+            blue = b - 0.2;
+            wait(0.1);
         } else {
             red = r;
             green = g;
             blue = b;
-            wait(0.25);
+            wait(0.4);
         }
         step += stepSize;
     } 
@@ -69,17 +70,17 @@ void thunderstorm(float r, float g, float b, float stepSize, float duration) {
     int lightning = 0;
     while (step < duration) {
         if (red == r && green == g && blue == b) {
-            red = 0.0;
-            green = 0.0;
-            blue = 0.0;
-            wait(0.25);
+            red = r;
+            green = g;
+            blue = b - 0.2;
+            wait(0.05);
         } else {
             red = r;
             green = g;
             blue = b;
-            wait(0.25);
+            wait(0.2);
         }
-        lightning = rand()%6 + 1;
+        lightning = rand()%8 + 1;
         if (lightning == 1) {
             red = 1.0;
             green = 1.0;
@@ -89,22 +90,89 @@ void thunderstorm(float r, float g, float b, float stepSize, float duration) {
         step += stepSize;
     } 
 } 
+
+void sunset(){
+    fade(1.0, 0.2, 0.0, 0.001, 0.00); //fade to sunset
+    while(1){
+    }
+}
+
+void night(){
+    fade(0.1, 0.0, 0.25, 0.001, 0.00); //fade to night
+    while(1){
+    }
+}
+
+void sunny(){
+    fade(1.0, 0.3, 0.0, 0.001, 0.00); //fade to sunny
+    while(1){
+    }
+}
+
+void winter(){
+    fade(1.0, 1.0, 0.3, 0.001, 0.00); //fade to winter
+    while(1){
+    }
+}
+
+void rain() {
+    while(1) {
+        rain(0.1, 0.0, 0.35, 0.1, 4.0); //blink blue for rain
+    }
+    
+}
+
+void thunderstorm() {
+    while(1) {
+        thunderstorm(0.1, 0.0, 0.35, 0.1, 8.0); //blink blue and flash white for thunder
+    }
+}
+ void cloudy() {
+    while(1) {
+        fade(0.5, 0.5, 0.15, 0.001, 0.01); //fade to cloudy (half values of winter)
+        wait(2.5);
+        fade(0.35, 0.35, 0.1, 0.001, 0.01); //fade to slightly cloudy
+        wait(2.5);
+        fade(0.25, 0.25, 0.075, 0.001, 0.01); //fade to slightly cloudy
+        wait(2.5);
+        fade(0.1, 0.1, 0.03, 0.001, 0.01); //fade to slightly cloudy
+        wait(2.5);
+        fade(0.01, 0.008, 0.001, 0.001, 0.01); //fade to nearly cloudless
+        wait(2.5);
+        if (temp != 'c')
+            break; 
+    }    
+}
+
+void off() {
+    red = 0.0;
+    green = 0.0;
+    blue = 0.0;
+    while(1) {
+    }
+}
+
+void dev_recv()
+{
+    while(pi.readable()) {
+        temp = pi.getc();
+        if (temp=='l') sunny();
+        if (temp=='c') cloudy();
+        if (temp=='r') rain();
+        if (temp=='t') thunderstorm();
+        if (temp=='s') winter();
+        if (temp=='n') night();
+        if (temp=='b') sunset(); //sunrise
+        if (temp=='e') sunset();
+        if (temp == 'f') off();
+    }
+}
+
 int main() {
     red.period(0.01f);//setting the period of one PWMOut sets all of them
-
-    red = 1.0;
-    green = 1.0;
-    blue = 1.0;
+    pi.baud(9600);
+    pi.attach(&dev_recv, Serial::RxIrq);
     while(1) {
-        fade(1.0, 0.2, 0.0, 0.001, 0.00); //fade to sunset
-        wait(10.0);
-        fade(0.1, 0.0, 0.25, 0.001, 0.00); //fade to night
-        wait(10.0);
-        fade(1.0, 0.3, 0.0, 0.001, 0.00); //fade to sunny
-        wait(10.0);
-        fade(1.0, 1.0, 0.3, 0.001, 0.00); //fade to winter
-        wait(10.0);
-        rain(0.1, 0.0, 0.25, 0.1, 4.0); //blink blue for rain
-        thunderstorm(0.1, 0.0, 0.25, 0.1, 8.0); //blink blue and flash white for thunder
+        sleep();
     }
 }
