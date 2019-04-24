9521017f7d3bf161372b55e12b87bf716a4604df
syeomans <samuelyeomans@gmail.com>
2019-04-24 14:11:37 -0400

    Get data from javascript, send data to Mbed

--- cloudLamp.py
+++ cloudLamp.py
@@ -1,39 +1,82 @@
 # Import modules
 import time
-# import RPi.GPIO as GPIO
-#import urllib2
+import urllib3
+import certifi
 import json
 import serial
 import sys
 
-jsCondition = str(sys.argv[1])
-print(jsCondition)
-sys.stdout.flush()
+# Get the condition from javascript
+jsMode = str(sys.argv[1])
+jsRed = str(sys.argv[2])
+jsGreen = str(sys.argv[3])
+jsBlue = str(sys.argv[4])
+jsWeather = str(sys.argv[5])
 
 
 def getWeather():
 	"""
 	Make an api call to openweathermap.org
-
-	Returns one of:
-	['Clear', 'Drizzle', 'Clouds', 'Thunderstorm', 'Rain', 'Snow', 'Fog', 'Mist', 'Smoke', 'Haze']
-		(Technically it can return more, but these are the only ones we care about. 
-		Link to see the rest: https://openweathermap.org/weather-conditions )
 	"""
+	# Set up a pool manager for urllib3 with certificates
+	http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
+
+	# Generate API request
 	apiKey = "1be18276dbf6dd7f92eb0d144ef88985"
-	url = "https://api.openweathermap.org/data/2.5/weather?id=4671576&APPID=" + apiKey
+	url = "https://api.openweathermap.org/data/2.5/weather?id=4180439&APPID=" + apiKey
+	request = http.request('GET', url)
+	data = json.loads(request.data)
 
-	request = urllib2.Request(url)
-	contents = json.load(urllib2.urlopen(request))
-	return(contents['weather'][0]['main'])
+	# Parse the data we care about
+	weatherData = data['weather'][0]['main']
+	sunriseTime = int(data['sys']['sunrise'])
+	sunsetTime = int(data['sys']['sunset'])
 
-# # Main loop
-# currentWeather = getWeather()
-# print(currentWeather)
+	# Get the current time
+	currentTime = time.time()
 
-ser = serial.Serial('/dev/ttyACM0')  # open serial port
-print(ser.name)         # check which port was really used
-ser.write(jsCondition)     # write a string
-ser.close()             # close port
+	# Generate the single character to send to the Mbed
+	weatherChar = ""
+	if weatherData == "Clear":
+		if sunriseTime - currentTime > 600 or sunsetTime - currentTime < 0:
+			weatherChar = "n"
+		elif sunriseTime - currentTime <= 600 and sunriseTime - currentTime > 0:
+			weatherChar = "b"
+		elif sunsetTime - currentTime <= 600 and sunsetTime - currentTime > 0:
+			weatherChar = "e"
+		else:
+			weatherChar = "L"
+	elif weatherData in ["Drizzle", "Rain"]:
+		weatherChar = "r"
+	elif weatherData in ["Clouds", "Fog", "Mist", "Smoke", "Haze"]:
+		weatherChar = "c"
+	elif weatherData == "Thunderstorm":
+		weatherChar = "t"
+	elif weatherData == "Snow":
+		weatherChar = "s"
+	else:
+		weatherChar = "L"
 
+	# Return the character
+	return(weatherChar)
 
+if jsMode == "f":
+	ser = serial.Serial('/dev/ttyACM0')  # open serial port
+	ser.write(jsMode)     # write a string
+	ser.close()             # close port
+elif jsMode == "d":
+	ser = serial.Serial('/dev/ttyACM0')  # open serial port
+	ser.write(jsWeather)     # write a string
+	ser.close()             # close port
+elif jsMode == "w":
+	currentWeather = getWeather()
+	ser = serial.Serial('/dev/ttyACM0')  # open serial port
+	ser.write(currentWeather)     # write a string
+	ser.close()             # close port
+elif jsMode == "u":
+	ser = serial.Serial('/dev/ttyACM0')  # open serial port
+	ser.write(jsMode)     # write a string
+	ser.write(jsRed)
+	ser.write(jsGreen)
+	ser.write(jsBlue)
+	ser.close()             # close port
